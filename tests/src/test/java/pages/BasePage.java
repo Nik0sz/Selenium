a9876d4e70b3642f83a18d81e17cd452d9c9b79b
Nik0sz <denkler.nikosz@gmail.com>
2025-05-07 17:29:02 +0200

    setup

---
+++ tests/src/test/java/pages/BasePage.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasePage {
+    protected final WebDriver driver;
+    private final WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait   = new WebDriverWait(driver, 10);   
+    }
+
+    protected WebElement waitForVisible(final By locator) {
+        return wait.until(new ExpectedCondition<WebElement>() {
+            public WebElement apply(WebDriver d) {
+                WebElement el = d.findElement(locator);
+                return (el.isDisplayed()) ? el : null;
+            }
+        });
+    }
+
+    public String getTitle() {
+        return driver.getTitle();
+    }
+
+    protected WebElement waitAndReturnElement(By locator) {
+        this.wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return this.driver.findElement(locator);
+    } 
+    
+    public String getBodyText() {
+        WebElement bodyElement = this.waitAndReturnElement(By.tagName("body"));
+        return bodyElement.getText();
+    }
+
+}

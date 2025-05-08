32a56f3452526f6ad10d0e0d1c8f70b8f88d89d5
Nik0sz <denkler.nikosz@gmail.com>
2025-05-08 16:34:33 +0200

    minor inconvinienc removed

--- tests/src/test/java/tests/ChallengingDomTests.java
+++ tests/src/test/java/tests/ChallengingDomTests.java
@@ -10,6 +10,6 @@ public class ChallengingDomTests extends BaseTest {
     public void complexXpathEditButtonIsClickable() {
         ChallengingDomPage dom = new HomePage(driver).openChallengingDom();
         dom.clickCrazyEditLink();
-        // nothing to assert - if it wasn't found or clickable the test fails
+        // nothing to assert if it wasn't found or clickable the test fails
     }
 }
